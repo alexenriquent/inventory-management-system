5ab7d275a8e6deb8af02aa977635ef12f8add651
Thanat Chokwijitkul <alexenriquent@gmail.com>
2016-10-13 13:18:25 +1000

    Implemented T(s,a,s')

--- src/solver/MySolver.java
+++ src/solver/MySolver.java
@@ -28,10 +28,14 @@ public class MySolver implements OrderingAgent {
 	public List<Integer> generateStockOrder(List<Integer> stockInventory, int numWeeksLeft) {
 		List<Integer> itemOrders = generateAction(stockInventory);
 				
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 5; i++) {
 			List<Integer> a = generateAction(stockInventory);
-			System.out.println(a);
-			System.out.println(rewardFunction(stockInventory, a));
+			System.out.println("\nS = " + stockInventory);
+			System.out.println("A = " + a);
+			System.out.println("R(s) = " + rewardFunction(stockInventory));
+			System.out.println("R(s,a) = " + rewardFunction(stockInventory, a));
+			System.out.println("T(s,a,s') = " + transitionFunction(stockInventory, a));
+			System.out.println();
 		}
 		
 		return itemOrders;
@@ -59,6 +63,28 @@ public class MySolver implements OrderingAgent {
 		return itemOrders;
 	}
 	
+	public double transitionFunction(List<Integer> stockInventory, List<Integer> itemOrders) {
+		double totalTransitionProbability = 1.0;
+		List<Integer> updatedInventory = nextState(stockInventory, itemOrders);
+		
+		for (int i = 0; i < store.getMaxTypes(); i++) {
+			double transitionProbability = 0.0;
+			int totalItems = stockInventory.get(i) + itemOrders.get(i);
+			if (updatedInventory.get(i) > totalItems) {
+				transitionProbability = 0.0;
+			} else if (updatedInventory.get(i) > 0 && updatedInventory.get(i) <= totalItems) {
+				transitionProbability = probabilities.get(i).get(totalItems, totalItems - updatedInventory.get(i));
+			} else if (updatedInventory.get(i) == 0) {
+				for (int j = totalItems; j < store.getCapacity(); j++) {
+					transitionProbability += probabilities.get(i).get(totalItems, j);
+				}
+			}
+			totalTransitionProbability *= transitionProbability;
+		}
+		
+		return totalTransitionProbability;
+	}
+	
 	public double rewardFunction(List<Integer> stockInventory, List<Integer> itemOrders) {
 		double totalReward = 0.0;
 		
@@ -72,6 +98,30 @@ public class MySolver implements OrderingAgent {
 		}
 		
 		return totalReward;
+	}
+	
+	public double rewardFunction(List<Integer> stockInventory) {
+		double totalReward = 0.0;
+		
+		for (int i = 0; i < store.getMaxTypes(); i++) {
+			double reward = 0.0;
+			for (int j = stockInventory.get(i) + 1; j < store.getCapacity(); j++) {
+				reward = j - stockInventory.get(i) * (probabilities.get(i).get(stockInventory.get(i), j));
+			}
+			totalReward += -1 * reward;
+		}
+		
+		return totalReward;
+	}
+	
+	public List<Integer> nextState(List<Integer> stockInventory, List<Integer> itemOrders) {
+		List<Integer> updatedInventory = new ArrayList<Integer>();
+		
+		for (int i = 0; i < store.getMaxTypes(); i++) {
+			updatedInventory.add(stockInventory.get(i) + itemOrders.get(i));
+		}
+		
+		return updatedInventory;
 	}
 
 }
